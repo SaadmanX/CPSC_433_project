0c75bfdd4ca2a4beaf11f3aab4236ec1f51da804
nhat2105 <ygopro113@gmail.com>
2024-12-05 12:57:22 -0700

    newest SoftConstraints added, still need polish

--- src/constraints/SoftConstraintsEval.java
+++ src/constraints/SoftConstraintsEval.java
@@ -1,6 +1,5 @@
 package constraints;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,32 +10,29 @@ import model.task.Task;
 import model.constraints.Pair;
 import model.constraints.Preference;
 
-//TODO: Something wrong, beacause penaltyList is unused, have to check this again 
-//TODO: (it's another multiplier in the subfunction), and all these values must be calculated separately
-//TODO: WIll need to separate it into subfunctions for this reason + readibility
+//TODO: (prefList another multiplier in the subfunction), and all these values must be calculated separately
+
 public class SoftConstraintsEval {
     // Multiplier List (Order: minFill, pref, pair, secdiff)
     private List<Integer> multiplierList;
     
     // Penalty List (Order: gameMin, practiceMin, notPaired, section)
-    private List<Integer> penaltyList;
+    //private List<Integer> penaltyList;
 
-    private List<Preference> preferenceList;
+   //private List<Preference> preferenceList;
 
-    private List<Slot> allSlots;
+   // private List<Slot> allSlots;
 
     public int initialPenalty; //can be higher/lower after assignment, acts as a reference point of initial state
 
-    private List<Pair> pairs;
+    //private List<Pair> pairs;
 
     public SoftConstraintsEval(List<Integer> multiplierList, List<Integer> penaltyList, List<Preference> prefer, List<Pair> pair, List<Slot> allSlots) {
         this.multiplierList = multiplierList;
-        this.penaltyList = penaltyList;
-        this.preferenceList = prefer;
-        this.allSlots = allSlots;
-        this.pairs = pair;
-
-        initialPenalty = calculateInitialPenalty();
+        //this.penaltyList = penaltyList;
+        //this.preferenceList = prefer;
+        //this.allSlots = allSlots;
+        //this.pairs = pair;
     }
 
     //max 3 here for the link transit
@@ -57,15 +53,42 @@ public class SoftConstraintsEval {
         Task task = assignment.getTask();
 
         // Min Fill Penalty
-        if (slot.getCurrentCount() < slot.getMin()){
-            penalty += (slot.getMin() - slot.getCurrentCount()) * multiplierList.get(0);
-        }
+        penalty += evalMinFilled(slot);
 
         // Pref Penalty update
-        if (task.isPreferredSlot(slot))penalty -=  task.getPreferenceValue(slot) * multiplierList.get(1);
+        penalty += evalPreferencePenalty(task, slot);
 
         // Pairing
-        // Basically this needs to be iterated... can't think of another way, but this is so slow
+        penalty += evalPairingPenalty(task);
+                
+        // Section Difference Penalty
+        penalty += evalSecDiff(task, slot);
+                
+        return penalty;
+                    
+    }    
+
+    private boolean isOverlap(Task a, Task b){
+        return (a.getCurrentAssigned().getStartTime().equals(b.getCurrentAssigned().getStartTime()));
+    }
+
+    private int evalMinFilled(Slot slot){
+        if (slot.getCurrentCount() < slot.getMin()){
+            return (slot.getMin() - slot.getCurrentCount()) * multiplierList.get(0);
+        }
+
+        return 0;
+    }
+
+    private int evalPreferencePenalty(Task task, Slot slot){
+        if (task.isPreferredSlot(slot)){
+            
+            return (task.getSumPreferences() - task.getPreferenceValue(slot)) * multiplierList.get(1);
+        }
+        return 0;
+    }
+
+    private int evalPairingPenalty(Task task){
         int counter = 0;
         if (task.getPairs() != null){
             int total = task.getPairs().size();
@@ -76,15 +99,13 @@ public class SoftConstraintsEval {
                 }
             }
 
-            penalty += (total - counter) * multiplierList.get(2);
+            return (total - counter) * multiplierList.get(2);
         }
-                
-        // Section Difference Penalty
 
-        //Different divisional games within a single age/tier group should be scheduled 
-        //at different times. For each pair of divisions that is scheduled into the same slot, 
-        //we add a penalty pensection to the Eval-value of an assignment.
+        return 0;
+    }
 
+    private int evalSecDiff(Task task, Slot slot){
         HashMap<String, Integer> ageFrequencyMap = new HashMap<>();
 
         for (Task t : slot.getAssignedTasks()) {
@@ -102,58 +123,6 @@ public class SoftConstraintsEval {
                 numberOfPair += n * (n-1) / 2;
             }
         }
-        penalty += numberOfPair * multiplierList.get(3);
-                
-        return penalty;
-                    
-    }    
-
-    private boolean isOverlap(Task a, Task b){
-        return (a.getCurrentAssigned().getStartTime().equals(b.getCurrentAssigned().getStartTime()));
+        return numberOfPair * multiplierList.get(3);
     }
-
-
-    //Should only be called exactly once at the initial state, after only the newest assignment is evaluated
-    public int calculateInitialPenalty() {
-        int penalty = 0;
-
-        penalty += minFillPenalty() * multiplierList.get(0); 
-        //System.out.println("penalty after min fill check: " + Integer.toString(penalty));
-
-        penalty += preferencesPenalty() * multiplierList.get(1); 
-        //penalty += preferencesPenalty() * multiplierList.get(1); 
-        //System.out.println("penalty after preference check: " + Integer.toString(penalty));
-
-        penalty += pairingPenalty() * multiplierList.get(2); 
-        //System.out.println("penalty after pair check: " + Integer.toString(penalty));
-
-        //No initial assignment, no penalty for sec diff
-
-        return penalty;
-    }
-
-
-    private int minFillPenalty() {
-        int penalty = 0;
-        for (Slot slot : allSlots) {
-            penalty += slot.getMin();
-        }
-
-        return penalty;
-    }
-
-    private int preferencesPenalty() {
-        int penalty = 0;
-
-        for (Preference pref : preferenceList) {
-            penalty += pref.getPenalty();
-        }
-
-        return penalty;
-    }
-
-    private int pairingPenalty() {
-       return pairs.size();
-    }
-
 }
